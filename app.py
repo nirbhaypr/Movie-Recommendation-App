6642ff455fb8486fc80d85ea8a159dac364a36cb
Nirbhay Rathod <nirbhayp098@gmail.com>
2024-08-24 21:27:22 +0530

    The python source file

---
+++ app.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:339ed3fbf2ee9777115957dc3f62614cfd4d6af58db75eec7d7c1c850cdb9015
+size 5856
---
+++ rec.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:847bf5707db2acda77c008cce60886c94b162a0b4d34df0f4f9ff23c9459bfea
+size 2181
